0e7ced5fde9865fd8f30070a1953fb792f04fc20
Crissouza1995 <cristianfabiansouza@gmail.com>
2023-06-09 14:54:07 -0300

    Primercomit

--- script/script.js
+++ script/script.js
@@ -1,211 +1,191 @@
-
-
+const sabadoTrabajado = document.querySelector('#sabados-trabajados');
+const extrasUsuario = document.querySelector('#horas-extras');
 
 function calcularSueldo() {
+  let categoriaSeleccionada = "";
+  let horasExtras = 0;
+  let sabadoUsuario = 0;
+  const tecnico1 = 700;
+  const tecnico2 = 900;
+  const supervisorProduccion = 1400;
+  const jefeDeSector = 1900;
 
-  document.addEventListener('DOMContentLoaded', () => {
+  const usuariosRegistrados = []; // Array de usuarios registrados
 
+  const categoriaSelector = document.querySelector('#categoria');
+  categoriaSelector.addEventListener('change', () => {
+    categoriaSeleccionada = categoriaSelector.value;
+  });
 
-    var categoriaSeleccionada = "";
-    let horasExtras = 0;
-    let sabadoUsuario = 0;
-    let tecnico1 = 700;
-    let tecnico2 = 900;
-    let supervisorProduccion = 1400;
-    let jefeDeSector = 1900;
-    let sueldoBasico = 0;
-    let sueldoTotal = 0;
+  const validNombre = document.querySelector('#nombre-usuario');
+
+  const validarBtnUser = document.querySelector('#validar-nombre');
+
+  validarBtnUser.addEventListener('click', () => {
+    const nombreUsuario = document.querySelector('#nombre-usuario').value;
+
+    validNombre.classList.add("is-valid");
+
+    usuariosRegistrados[1] = nombreUsuario;
+  });
+
+  const validarBtnHorasExtras = document.querySelector('#validar-horas-extras');
+
+  validarBtnHorasExtras.addEventListener('click', () => {
+
     
+    const extraError = ('#extraError');
+    horasExtras = document.querySelector('#horas-extras').value;
 
+    if (horasExtras < 0 || horasExtras > 40) {
 
-    const usuariosRegistrados = []; // Array de usuarios registrados
+      event.preventDefault();
+      const extraError = document.querySelector('#extraError');
+      extraError.textContent = "No se pueden realizar más de 40 horas extras en el mes";
+      extrasUsuario.classList.add("is-invalid");
+      return;
+    }
 
-    // validamos los datos ingresados
+    extraError.textContent = "";
+    extrasUsuario.classList.remove("is-invalid");
+    extrasUsuario.classList.add("is-valid");
 
-    const categoriaSelector = document.querySelector('#categoria');
-    categoriaSelector.addEventListener('change', () => {
-      categoriaSeleccionada = categoriaSelector.value;
 
-      usuariosRegistrados.push(categoriaSeleccionada);
-
-    });
-
-
-
-
-    const validNombre = document.querySelector('#nombre-usuario');
-    const validarBtnUser = document.querySelector('#validar-nombre');
-    validarBtnUser.addEventListener('click', () => {
-
-      const nombreUsuario = document.querySelector('#nombre-usuario').value;
-
-
-      validNombre.classList.add("is-valid");
-
-      usuariosRegistrados.push(nombreUsuario);
-
-    });
-
-
-
-    const validarBtnHorasExtras = document.querySelector('#validar-horas-extras');
-
-
-    validarBtnHorasExtras.addEventListener('click', () => {
-
-      const extrasUsuario = document.querySelector('#horas-extras');
-      const extraError = ('#extraError');
-
-      horasExtras = document.querySelector('#horas-extras').value;
-
-
-
-      if (horasExtras < 0 || horasExtras > 40) {
-
-        event.preventDefault();
-        extraError.textContent = "No se pueden realizar mas de 40hs extras en el mes";
-        extrasUsuario.classList.add("is-invalid")
-        return;
-      }
-
-      extraError.textContent = "";
-      extrasUsuario.classList.remove("is-invalid");
-      extrasUsuario.classList.add("is-valid");
-
-      usuariosRegistrados.push(horasExtras);
-
-
-    });
-
-
-    const sabadoTrabajado = document.querySelector('#sabados-trabajados');
-
-
-    const validarSabadosUsuario = document.querySelector('#validar-sabado');
-    validarSabadosUsuario.addEventListener('click', () => {
-
-      sabadoUsuario = document.querySelector('#sabados-trabajados').value;
-
-      const sabadoError = document.querySelector('#sabadoError');
-
-      if (sabadoUsuario > 4) {
-        event.preventDefault();
-        sabadoError.textContent = "Es imposible que 1 mes tenga mas de 4 sabadados.";
-        sabadoTrabajado.classList.add("is-invalid");
-        return;
-      }
-      sabadoError.textContent = "";
-      sabadoTrabajado.classList.remove("is-invalid");
-      sabadoTrabajado.classList.add("is-valid");
-
-      usuariosRegistrados.push(sabadoUsuario)
-
-
-    });
-
-   
-    // trabajamos de acuerdo los datos ingresados
-
-    //1)En caso de haber seleccionado Tecnico de primera categoria--
-
-
-    const calcularBtn = document.querySelector('#calcular-btn');
-    calcularBtn.addEventListener('click', () => {
-
-      categoriaSeleccionada = parseInt(categoriaSeleccionada);
-
-
-     
-      
-      switch (categoriaSeleccionada) {
-
-
-      
-        case 1:
-          sueldoBasico = horasExtras * tecnico1;
-          sueldoBasico = sueldoBasico * 2; // se multiplica por 2 ya que las horas extras se pagan un 50% mas que las horas normales.
-          sueldoTotal = sueldoBasico + sabadoUsuario * 4000; // el valor de sabados por la categoriaseleccionada
-          break;
-
-        case 2:
-
-          sueldoBasico = horasExtras * tecnico2;
-          sueldoBasico = sueldoBasico * 2; // se multiplica por 2 ya que las horas extras se pagan un 50% mas que las horas normales.
-          sueldoTotal = sueldoBasico + sabadoUsuario * 6000; // el valor de sabados por la categoriaseleccionada
-          break;
-
-        case 3:
-
-          sueldoBasico = horasExtras * supervisorProduccion;
-          sueldoBasico = sueldoBasico * 2; // se multiplica por 2 ya que las horas extras se pagan un 50% mas que las horas normales.
-          sueldoTotal = sueldoBasico + sabadoUsuario * 8000; // el valor de sabados por la categoriaseleccionada
-          break;
-
-        case 4:
-          sueldoBasico = horasExtras * jefeDeSector;
-          sueldoBasico = sueldoBasico * 2; // se multiplica por 2 ya que las horas extras se pagan un 50% mas que las horas normales.
-          sueldoTotal = sueldoBasico + sabadoUsuario * 10000; // el valor de sabados por la categoriaseleccionada
-          break;
-      }
-
-      usuariosRegistrados.push(sueldoTotal);
-      console.log(sueldoTotal);
-      console.log(usuariosRegistrados)
-
-
-      const arrayEmpleado = usuariosRegistrados.map(e=>{
-
-        return{
-
-          categoria: usuariosRegistrados[0],
-          nombre: usuariosRegistrados[1],
-          horasExtras: usuariosRegistrados[2],
-          sabadoTrabajado: usuariosRegistrados[3],
-          salario: usuariosRegistrados[4],
-
-        }
-
-      });
-    
-      console.log(arrayEmpleado);
-
-      const empleadoTabla = ()=>{
-
-        const tablaBody = document.querySelector("#tabla-main");
-    
-        arrayEmpleado.forEach(empleado => {
-
-          const fila = document.createElement("tr");
-          const columnaNombre = document.createElement("td");
-          columnaNombre.textContent = empleado.nombre;
-          const columnaCategoria = document.createElement("td");
-          columnaCategoria.textContent = empleado.categoria;
-          const columnaSueldo = document.createElement("td");
-          columnaSueldo.textContent = empleado.salario;
-          const botonEliminar = document.createElement('button');
-          botonEliminar.textContent ='Eliminar';
-    
-          fila.appendChild(columnaNombre);
-          fila.appendChild(columnaCategoria);
-          fila.appendChild(columnaSueldo);
-          fila.appendChild(botonEliminar)
-          tablaBody.appendChild(fila);
-    
-        });
-    
-      }
-
-      empleadoTabla();
-
-    });
+    usuariosRegistrados[2] = horasExtras;
   });
 
 
+  const validarSabadosUsuario = document.querySelector('#validar-sabado');
 
+  validarSabadosUsuario.addEventListener('click', () => {
+    sabadoUsuario = document.querySelector('#sabados-trabajados').value;
+
+    const sabadoError = document.querySelector('#sabadoError');
+
+    if (sabadoUsuario > 4) {
+      event.preventDefault();
+      const sabadoError = document.querySelector('#sabadoError');
+      sabadoError.textContent = "Es imposible que 1 mes tenga más de 4 sábados.";
+      sabadoTrabajado.classList.add("is-invalid");
+      return;
+    } else {
+      sabadoError.textContent = "";
+      sabadoTrabajado.classList.remove("is-invalid");
+      sabadoTrabajado.classList.add("is-valid");
+    }
+
+    usuariosRegistrados.push(sabadoUsuario)
+
+    usuariosRegistrados[3] = sabadoUsuario;
+  });
+
+  const calcularBtn = document.querySelector('#calcular-btn');
+
+  calcularBtn.addEventListener('click', () => {
+    calcularEmpleado();
+    limpiarCampos();
+  });
+
+  function calcularEmpleado() {
+    event.preventDefault();
+    categoriaSeleccionada = parseInt(categoriaSeleccionada);
+
+    let sueldoBasico = 0;
+    let sueldoTotal = 0;
+
+    debugger
+
+    switch (categoriaSeleccionada) {
+      case 1:
+        if (categoriaSeleccionada == 1) {
+          categoriaSeleccionada = "Tecnico de Primera Categoria"
+        }
+        sueldoBasico = horasExtras * tecnico1 * 2;
+        sueldoTotal = sueldoBasico + sabadoUsuario * 4000;
+        break;
+      case 2:
+        if (categoriaSeleccionada == 2) {
+          categoriaSeleccionada = "Tecnico de Segunda Categoria"
+        }
+        sueldoBasico = horasExtras * tecnico2 * 2;
+        sueldoTotal = sueldoBasico + sabadoUsuario * 6000;
+        break;
+      case 3:
+        if (categoriaSeleccionada == 3) {
+          categoriaSeleccionada = "Supervisor de Produccion"
+        }
+        sueldoBasico = horasExtras * supervisorProduccion * 2;
+        sueldoTotal = sueldoBasico + sabadoUsuario * 8000;
+        break;
+      case 4:
+        if (categoriaSeleccionada == 4) {
+          categoriaSeleccionada = "Jefe de Sector"
+        }
+        sueldoBasico = horasExtras * jefeDeSector * 2;
+        sueldoTotal = sueldoBasico + sabadoUsuario * 10000;
+        break;
+    }
+
+    usuariosRegistrados[0] = categoriaSeleccionada;
+
+    usuariosRegistrados[4] = sueldoTotal;
+
+    const empleado = {
+      categoria: usuariosRegistrados[0],
+      nombre: usuariosRegistrados[1],
+      horasExtras: usuariosRegistrados[2],
+      sabadoTrabajado: usuariosRegistrados[3],
+      salario: usuariosRegistrados[4],
+    };
+
+    const tablaBody = document.querySelector("#tabla-main");
+    const fila = document.createElement("tr");
+    const columnaNombre = document.createElement("td");
+    columnaNombre.textContent = empleado.nombre;
+    const columnaCategoria = document.createElement("td");
+    columnaCategoria.textContent = empleado.categoria;
+    const columnaSueldo = document.createElement("td");
+    columnaSueldo.textContent = empleado.salario;
+    const botonEliminar = document.createElement('button');
+    botonEliminar.textContent = 'Eliminar';
+
+    fila.appendChild(columnaNombre);
+    fila.appendChild(columnaCategoria);
+    fila.appendChild(columnaSueldo);
+    fila.appendChild(botonEliminar);
+    tablaBody.appendChild(fila);
+  }
+
+
+
+  function limpiarCampos() {
+
+    usuariosRegistrados.length = 0;
+    categoriaSeleccionada = "";
+    horasExtras = 0;
+    sabadoUsuario = 0;
+    usuariosRegistrados.length = 0;
+
+    document.querySelector('#categoria').selectedIndex = 0;
+    document.querySelector('#nombre-usuario').value = "";
+    document.querySelector('#horas-extras').value = "";
+    document.querySelector('#sabados-trabajados').value = "";
+    document.querySelector('#extraError').textContent = "";
+    document.querySelector('#sabadoError').textContent = "";
+    debugger
+    validNombre.classList.remove("is-valid");
+    extrasUsuario.classList.remove("is-valid");
+    sabadoTrabajado.classList.remove("is-valid");
+
+  }
 }
 
 
-calcularSueldo();
+document.addEventListener('DOMContentLoaded', () => {
+
+  calcularSueldo();
+
+});
 
 
 
@@ -250,29 +230,9 @@ const buscarBtn = document.querySelector('#buscar-usuario');
     }
   }
   
-  const empleado1 = new Empleado("Carlos", "Flores", 271, "Tecnico de primera categoria");
-  const empleado2 = new Empleado("Carlos", "Toloza", 25, "Tecnico de segunda categoria");
-  const empleado3 = new Empleado("Carlos", "Gonzales", 271, "Supervisor de produccion");
-  const empleado4 = new Empleado("Luis", "Pereyra", 271, "Tecnico de segunda categoria");
-  const empleado5 = new Empleado("Alexis", "Rivarola", 271, "Jefe de sector");
-  const empleadodinamico = new Empleado("", "", 0, "");
-
+ 
   
   
-  }
-  
-
-
-
-
-
-/*Storage y Json
-
-
-
-
-metodo => let user = sessionstorage.seItem(usuario, cristian);
-
 
 
 
