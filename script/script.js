f31da4c2a1de60d247b6a31e21d9fcc3131d632c
Crissouza1995 <cristianfabiansouza@gmail.com>
2023-06-19 19:30:46 -0300

    se optimizo codigo segun observaciones de tutor

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 const sabadoTrabajado = document.querySelector('#sabados-trabajados');
 const extrasUsuario = document.querySelector('#horas-extras');
-const arrayEmpleados = []
+const arrayEmpleados = [];
 
 function calcularSueldo() {
   let categoriaSeleccionada = "";
@@ -11,32 +11,30 @@ function calcularSueldo() {
   const supervisorProduccion = 1400;
   const jefeDeSector = 1900;
 
-  const usuariosRegistrados = []; // Array de usuarios registrados
+  const usuariosRegistrados = [];
 
   const categoriaSelector = document.querySelector('#categoria');
+  const validNombre = document.querySelector('#nombre-usuario');
+  const validarBtnUser = document.querySelector('#validar-nombre');
+  const validarBtnHorasExtras = document.querySelector('#validar-horas-extras');
+  const validarSabadosUsuario = document.querySelector('#validar-sabado');
+  const calcularBtn = document.querySelector('#calcular-btn');
+  const tablaBody = document.querySelector("#tabla-main");
+
   categoriaSelector.addEventListener('change', () => {
     categoriaSeleccionada = categoriaSelector.value;
   });
 
-  const validNombre = document.querySelector('#nombre-usuario');
-
-  const validarBtnUser = document.querySelector('#validar-nombre');
-
   validarBtnUser.addEventListener('click', () => {
     const nombreUsuario = document.querySelector('#nombre-usuario').value;
-
     validNombre.classList.add("is-valid");
-
     usuariosRegistrados[1] = nombreUsuario;
   });
 
-  const validarBtnHorasExtras = document.querySelector('#validar-horas-extras');
   validarBtnHorasExtras.addEventListener('click', () => {
-    const extraError = ('#extraError');
     horasExtras = document.querySelector('#horas-extras').value;
 
     if (horasExtras < 0 || horasExtras > 40) {
-
       event.preventDefault();
       const extraError = document.querySelector('#extraError');
       extraError.textContent = "No se pueden realizar más de 40 horas extras en el mes";
@@ -44,6 +42,7 @@ function calcularSueldo() {
       return;
     }
 
+    const extraError = document.querySelector('#extraError');
     extraError.textContent = "";
     extrasUsuario.classList.remove("is-invalid");
     extrasUsuario.classList.add("is-valid");
@@ -51,16 +50,12 @@ function calcularSueldo() {
     usuariosRegistrados[2] = horasExtras;
   });
 
-  const validarSabadosUsuario = document.querySelector('#validar-sabado');
-
   validarSabadosUsuario.addEventListener('click', () => {
     sabadoUsuario = document.querySelector('#sabados-trabajados').value;
-
     const sabadoError = document.querySelector('#sabadoError');
 
     if (sabadoUsuario > 4) {
       event.preventDefault();
-      const sabadoError = document.querySelector('#sabadoError');
       sabadoError.textContent = "Es imposible que 1 mes tenga más de 4 sábados.";
       sabadoTrabajado.classList.add("is-invalid");
       return;
@@ -69,11 +64,10 @@ function calcularSueldo() {
       sabadoTrabajado.classList.remove("is-invalid");
       sabadoTrabajado.classList.add("is-valid");
     }
-    usuariosRegistrados.push(sabadoUsuario)
+
     usuariosRegistrados[3] = sabadoUsuario;
   });
 
-  const calcularBtn = document.querySelector('#calcular-btn');
   calcularBtn.addEventListener('click', () => {
     calcularEmpleado();
     limpiarCampos();
@@ -85,41 +79,32 @@ function calcularSueldo() {
 
     let sueldoBasico = 0;
     let sueldoTotal = 0;
-
-// instancia para evaluar la categoria seleccionada del empleado 
+    let categoriaTexto = "";
 
     switch (categoriaSeleccionada) {
       case 1:
-        if (categoriaSeleccionada == 1) {
-          categoriaSeleccionada = "Tecnico de Primera Categoria"
-        }
+        categoriaTexto = "Tecnico de Primera Categoria";
         sueldoBasico = horasExtras * tecnico1 * 2;
         sueldoTotal = sueldoBasico + sabadoUsuario * 4000;
         break;
       case 2:
-        if (categoriaSeleccionada == 2) {
-          categoriaSeleccionada = "Tecnico de Segunda Categoria"
-        }
+        categoriaTexto = "Tecnico de Segunda Categoria";
         sueldoBasico = horasExtras * tecnico2 * 2;
         sueldoTotal = sueldoBasico + sabadoUsuario * 6000;
         break;
       case 3:
-        if (categoriaSeleccionada == 3) {
-          categoriaSeleccionada = "Supervisor de Produccion"
-        }
+        categoriaTexto = "Supervisor de Produccion";
         sueldoBasico = horasExtras * supervisorProduccion * 2;
         sueldoTotal = sueldoBasico + sabadoUsuario * 8000;
         break;
       case 4:
-        if (categoriaSeleccionada == 4) {
-          categoriaSeleccionada = "Jefe de Sector"
-        }
+        categoriaTexto = "Jefe de Sector";
         sueldoBasico = horasExtras * jefeDeSector * 2;
         sueldoTotal = sueldoBasico + sabadoUsuario * 10000;
         break;
     }
 
-    usuariosRegistrados[0] = categoriaSeleccionada;
+    usuariosRegistrados[0] = categoriaTexto;
     usuariosRegistrados[4] = sueldoTotal;
     const empleado = {
       categoria: usuariosRegistrados[0],
@@ -129,13 +114,8 @@ function calcularSueldo() {
       salario: usuariosRegistrados[4],
     };
 
-
-    //creo un nuevo array objeto para almacenar todos los usuarios en un array y etsos poder almacenarlos en el JSON 
-
     arrayEmpleados.push(empleado);
 
-    const tablaBody = document.querySelector("#tabla-main");
-    tablaBody.className ='table table-dark table-striped'
     const fila = document.createElement("tr");
     fila.className = 'align-items-center my-2';
     const columnaNombre = document.createElement("td");
@@ -156,32 +136,25 @@ function calcularSueldo() {
     tablaBody.appendChild(fila);
   }
 
-  debugger;
+  const arrayEmpleadosString = localStorage.getItem('empleados');
+  const arrayEmpleadosParsed = JSON.parse(arrayEmpleadosString);
 
-  // ---Local Storage---
-const arrayEmpleadosString = JSON.stringify(arrayEmpleados);
+  if (Array.isArray(arrayEmpleadosParsed) && arrayEmpleadosParsed.length > 0) {
+    arrayEmpleados.push(...arrayEmpleadosParsed);
+    console.log(arrayEmpleados);
+  }
 
-// Almaceno la cadena en el localStorage
-localStorage.setItem('empleados', arrayEmpleadosString);
+  function eliminarUsuario(event) {
+    const fila = event.target.parentNode;
+    const indice = Array.from(fila.parentNode.children).indexOf(fila);
+    arrayEmpleados.splice(indice, 1);
+    fila.remove();
 
-function eliminarUsuario(event) {
-  
-  const fila = event.target.parentNode;
-  
-  const indice = Array.from(fila.parentNode.children).indexOf(fila);
+    const arrayEmpleadosString = JSON.stringify(arrayEmpleados);
+    localStorage.setItem('empleados', arrayEmpleadosString);
+  }
 
-  arrayEmpleados.splice(indice, 1);
-
-  fila.remove();
-
-  // Actualizo el localStorage con el nuevo contenido de arrayEmpleados
-  const arrayEmpleadosString = JSON.stringify(arrayEmpleados);
-  localStorage.setItem('empleados', arrayEmpleadosString);
-}
-
-// Limpio los campos a completar
   function limpiarCampos() {
-
     usuariosRegistrados.length = 0;
     categoriaSeleccionada = "";
     horasExtras = 0;
@@ -194,27 +167,15 @@ function eliminarUsuario(event) {
     document.querySelector('#sabados-trabajados').value = "";
     document.querySelector('#extraError').textContent = "";
     document.querySelector('#sabadoError').textContent = "";
-   
+
     validNombre.classList.remove("is-valid");
     extrasUsuario.classList.remove("is-valid");
     sabadoTrabajado.classList.remove("is-valid");
-
   }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-
-  const arrayEmpleadosString = localStorage.getItem('empleados');
-
-  const arrayEmpleados = JSON.parse(arrayEmpleadosString);
-
-  if (Array.isArray(arrayEmpleados) && arrayEmpleados.length > 0) {
-    
-    console.log(arrayEmpleados);
-  }
-
   calcularSueldo();
-
 });
 
 
